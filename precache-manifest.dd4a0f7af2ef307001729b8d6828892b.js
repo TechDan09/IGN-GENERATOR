62998d5e009d7d355fd1526ee5b83aad0a9e590f
techdan09 <techdan09@gmail.com>
2019-07-30 19:04:43 +0100

    Updates

---
+++ precache-manifest.dd4a0f7af2ef307001729b8d6828892b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f750323eba406762aa423225087c5f",
+    "url": "/IGN-GENERATOR/index.html"
+  },
+  {
+    "revision": "0ed559dd38a068f6f75d",
+    "url": "/IGN-GENERATOR/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "07cd7874fc4a75293457",
+    "url": "/IGN-GENERATOR/static/css/main.597c3811.chunk.css"
+  },
+  {
+    "revision": "0ed559dd38a068f6f75d",
+    "url": "/IGN-GENERATOR/static/js/2.36f35a48.chunk.js"
+  },
+  {
+    "revision": "07cd7874fc4a75293457",
+    "url": "/IGN-GENERATOR/static/js/main.bb4a5c31.chunk.js"
+  },
+  {
+    "revision": "dae5e656b895570d163a",
+    "url": "/IGN-GENERATOR/static/js/runtime~main.cbbf4836.js"
+  }
+]);
